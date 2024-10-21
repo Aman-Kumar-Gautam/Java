d9d4eb679b8560e91eeedf1803804b6b35f1319f
Aman-Kumar-Gautam <amangautam862@gmail.com>
2024-10-21 22:51:22 +0530

    edited

---
+++ practice/corePractice/src/corePractice/B.java
@@ -0,0 +1,19 @@
+package corePractice;
+
+public class B {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		B a1 = new B();
+		a1.test();
+		a1.test();
+		a1.test();
+		a1.test();
+	}
+	
+	public void test() {
+
+		System.out.println(100);
+		
+	}
+}
